827fb8051e3b667425885a2749e7bb048e574776
User <useHomer@macbook-air-user-3.local>
2016-07-29 12:37:02 +0600

    Initial Commit

---
+++ KazPersona/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KazPersona
+//
+//  Created by Aigerim'sMac on 29.07.16.
+//  Copyright © 2016 n17r. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

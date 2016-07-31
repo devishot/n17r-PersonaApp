67fdfa42ce6e86ef627341e6f3499d3c75e01306
katayevaa <kataeva.aigerim@gmail.com>
2016-07-31 21:22:43 +0600

    there are plenty of attached labels books collection view, the image sizes will be fixed text view added table view added - will add another elements soon

--- KazPersona/ViewController.swift
+++ KazPersona/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var speakerProfileImageView: UIImageView!
+    @IBOutlet weak var speakerNameLable: UILabel!
+    @IBOutlet weak var speakerRoleLabel: UILabel!
+    
+    @IBOutlet weak var listOfBooksLabel: UILabel!
+    @IBOutlet weak var booksCollectionView: UICollectionView!
+
+    let booksImageArray = [UIImage(named: "book1"), UIImage(named: "book2"), UIImage(named: "book3"),  UIImage(named: "book4"), UIImage(named: "book5"),  UIImage(named: "book6"), UIImage(named: "book7")]
+    
+    @IBOutlet weak var personDescriptionLabel: UILabel!
+    @IBOutlet weak var personDescriptionTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        speakerProfileImageView.image = UIImage(named:("profile_speaker"))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 7
     }
-
+   
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+            
+            let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! booksCollectionViewCell
+            
+            cell.imageView.image = self.booksImageArray[indexPath.row]
+            return cell
+            
+        }
 
 }
 
---
+++ KazPersona/booksCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  booksCollectionViewCell.swift
+//  KazPersona
+//
+//  Created by Aigerim'sMac on 31.07.16.
+//  Copyright © 2016 n17r. All rights reserved.
+//
+
+import UIKit
+
+class booksCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}

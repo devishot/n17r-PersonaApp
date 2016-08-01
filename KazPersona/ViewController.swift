a633a8322d8e327b6d5ad9ab4e08cf03aa62d17a
Sattar Stamkulov <sattar.coder@gmail.com>
2016-08-01 20:18:43 +0600

    configured Firebase; added some fetch example

--- KazPersona/ViewController.swift
+++ KazPersona/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+
 
 class ViewController: UIViewController {
 
@@ -22,11 +24,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var personDescriptionLabel: UILabel!
     @IBOutlet weak var personDescriptionTextView: UITextView!
     
+    let userID: String! = "abaitasov"
+    
+    // [START define_database_reference]
+    var ref: FIRDatabaseReference!
+    // [END define_database_reference]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-       
+
+        // [START create_database_reference]
+        ref = FIRDatabase.database().reference()
+        // [END create_database_reference]
+        
         speakerProfileImageView.image = UIImage(named:("profile_speaker"))
+
+        // self.ref.child("event_persons/tedx_ala").child("3").setValue(["uid": userID!])
+        
+        // [START fetch_persons_detailed_info]
+        ref.child("persons").child(userID!).observeSingleEventOfType(.Value, withBlock: { (snapshot) in
+            // Get user value
+            let postDict = snapshot.value as! [String : AnyObject]
+            
+            let full_name = postDict["name"] as! String
+            let short_bio = postDict["short_bio"] as! String
+            
+            self.speakerNameLable.text = full_name
+            self.speakerRoleLabel.text = short_bio
+            
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+        // [END define_database_reference]
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {

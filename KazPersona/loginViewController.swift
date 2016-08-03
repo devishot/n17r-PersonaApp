d53dab50545281600b199d6a32242eda09d96bef
katayevaa <kataeva.aigerim@gmail.com>
2016-08-03 21:39:44 +0600

    added Login page

---
+++ KazPersona/loginViewController.swift
@@ -0,0 +1,34 @@
+//
+//  loginViewController.swift
+//  KazPersona
+//
+//  Created by Aigerim'sMac on 03.08.16.
+//  Copyright © 2016 n17r. All rights reserved.
+//
+
+import UIKit
+
+class loginViewController: UIViewController {
+
+    @IBOutlet weak var loginBackgroundImageView: UIImageView!
+    @IBOutlet weak var loginBlurVisualEffectView: UIVisualEffectView!
+    @IBOutlet weak var appTitleLabel: UILabel!
+    @IBOutlet weak var descriptionTitleLabel: UILabel!
+    
+    @IBAction func loginButton(sender: AnyObject) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}

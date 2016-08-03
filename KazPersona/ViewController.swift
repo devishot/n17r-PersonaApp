fe06546b8439f5a6e66ac2c130546fa4863424f5
Sattar Stamkulov <sattar.coder@gmail.com>
2016-08-03 11:48:59 +0600

    show Articles in UITableView

--- KazPersona/ViewController.swift
+++ KazPersona/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Firebase
 
 
-class ViewController: UIViewController,  UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var speakerProfileImageView: UIImageView!
     @IBOutlet weak var speakerNameLable: UILabel!
@@ -18,19 +18,24 @@ class ViewController: UIViewController,  UITableViewDelegate {
     
     @IBOutlet weak var listOfBooksLabel: UILabel!
     @IBOutlet weak var booksCollectionView: UICollectionView!
-
-    let booksImageArray = [UIImage(named: "book1"), UIImage(named: "book2"), UIImage(named: "book3"),  UIImage(named: "book4"), UIImage(named: "book5"),  UIImage(named: "book6"), UIImage(named: "book7")]
     
     @IBOutlet weak var personDescriptionLabel: UILabel!
     @IBOutlet weak var personDescriptionTextView: UITextView!
     
-    let userID: String! = "abaitasov"
+    @IBOutlet weak var linksTableView: UITableView!
+    
     
     // [START define_database_reference]
     var ref: FIRDatabaseReference!
     // [END define_database_reference]
     
+    let cellIdentifier = "CellIdentifier"
+    let userID: String! = "abaitasov"
     
+    var booksImageArray = [UIImage(named: "book1"), UIImage(named: "book2"), UIImage(named: "book3"),  UIImage(named: "book4"), UIImage(named: "book5"),  UIImage(named: "book6"), UIImage(named: "book7")]
+    var articles: [[String: String]] = []
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -38,27 +43,52 @@ class ViewController: UIViewController,  UITableViewDelegate {
         // [START create_database_reference]
         ref = FIRDatabase.database().reference()
         // [END create_database_reference]
-        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
         speakerProfileImageView.image = UIImage(named:("profile_speaker"))
 
-        // self.ref.child("event_persons/tedx_ala").child("3").setValue(["uid": userID!])
-        
-        // [START fetch_persons_detailed_info]
+        // [START read_data_once]
+        // fetch person full bio
         ref.child("persons").child(userID!).observeSingleEventOfType(.Value, withBlock: { (snapshot) in
-            // Get user value
-            let postDict = snapshot.value as! [String : AnyObject]
-            
-            let full_name = postDict["name"] as! String
-            let short_bio = postDict["short_bio"] as! String
-            
+
+            let postDict = snapshot.value as! [String : String]
+            let full_name = postDict["name"]
+            let short_bio = postDict["short_bio"]
+            let cover_url = postDict["cover_url"]
+
             self.speakerNameLable.text = full_name
             self.speakerRoleLabel.text = short_bio
-            
+
+            if let url = NSURL(string: cover_url!), data = NSData(contentsOfURL: url)
+            {
+                self.speakerProfileImageView.image = UIImage(data: data)
+            }
+
         }) { (error) in
             print(error.localizedDescription)
         }
-        // [END define_database_reference]
+        // [END read_data_once]
+        
+        // [START read_data_once]
+        // fetch person articles
+        ref.child("person_articles").child(userID!).observeSingleEventOfType(.Value, withBlock: { (snapshot) in
+
+            let postArray = snapshot.value as! [[String: String]]
+
+            self.articles = postArray
+
+            // LOG
+            print(self.articles)
+
+            self.linksTableView.reloadData()
+
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+        // [END read_data_once]
     }
+    
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     return 7
@@ -71,7 +101,39 @@ class ViewController: UIViewController,  UITableViewDelegate {
             cell.imageView.image = self.booksImageArray[indexPath.row]
             return cell
             
-        }
+    }
 
+    // MARK: Table View Data Source Methods
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! linksTableViewCell
+
+        // Fetch Article
+        let article = self.articles[indexPath.row] as [String: String]
+
+        // Configure Cell
+        cell.linksTitleLabel?.text = article["title"]
+
+        return cell
+    }
+
+
+    // MARK: Table View Delegate Methods
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        // Fetch Article
+        let article = self.articles[indexPath.row] as [String: String]
+
+        // LOG
+        print(indexPath.row, article)
+    }
+    
 }
 

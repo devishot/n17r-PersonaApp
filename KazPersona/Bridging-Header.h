efae462c63462d517e1eb67a8d3849c636c573f9
Sattar Stamkulov <sattar.coder@gmail.com>
2016-08-04 13:01:38 +0600

    some changes

---
+++ KazPersona/Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridging-Header.h
+//  KazPersona
+//
+//  Created by MacBook Pro on 04.08.16.
+//  Copyright © 2016 n17r. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+@import FBSDKCoreKit;
+@import FBSDKLoginKit;
+@import FBSDKShareKit;
+
+#endif /* Bridging_Header_h */

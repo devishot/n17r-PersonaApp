38f536da8a4229953dac52ef785c8a2ccc10756d
katayevaa <kataeva.aigerim@gmail.com>
2016-08-04 13:00:28 +0600

    added MainView controller and LeaderBoardTavbleViewCell and more

--- KazPersona/ViewController.swift
+++ KazPersona/ViewController.swift
@@ -41,6 +41,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
 
         // [START create_database_reference]

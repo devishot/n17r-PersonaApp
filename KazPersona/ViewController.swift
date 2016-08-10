966c719f54b2e4eb1f1b0b45d8a57efcfabc8eef
Sattar Stamkulov <sattar.coder@gmail.com>
2016-08-10 19:20:52 +0600

    added display feedbacks

--- KazPersona/ViewController.swift
+++ KazPersona/ViewController.swift
@@ -39,10 +39,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         if let text = leaveFeedbackTextView.text, let user = FIRAuth.auth()?.currentUser {
             // send
             self.saveFeedback(text, uid: user.uid, facebookFriendsCount: currentUserFacebookFriendsCount!, completion: {() -> Void in
-                // update table
-                // clear and hide keyboard
+                // clear textarea and hide keyboard
                 self.leaveFeedbackTextView.text = ""
-                //self.view.endEditing(true)
                 self.leaveFeedbackTextView.resignFirstResponder()
             })
         }
@@ -55,12 +53,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     // [END define_database_reference]
 
     let articleLinkCellID = "CellIdentifier"
+    let feedbackCellID = "feedbackCell"
+
     var personUID: String = "abaitasov"
     var personWithRate: [String: AnyObject] = [:]
 
     var booksImageArray = [UIImage(named: "book1"), UIImage(named: "book2"), UIImage(named: "book3"),  UIImage(named: "book4"), UIImage(named: "book5"),  UIImage(named: "book6"), UIImage(named: "book7")]
     var articles: [[String: String]] = []
-    
+    var feedbacks: [[String: AnyObject]] = []
+
     let logoIconsImageArray = [UIImage(named: "esquire_logo"), UIImage(named: "buro_logo"), UIImage(named: "chronicle_logo"), UIImage(named: "kursiv_logo")]
     let backgroundImagesArray = [UIImage(named:"bacground_link"), UIImage(named:"background_buro"), UIImage(named:"background_chronicle"), UIImage(named:"background_kursiv")]
 
@@ -86,13 +87,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         frame.size.height = contentSize.height
         personDescriptionTextView.frame = frame
         
-        
 
         // Do any additional setup after loading the view, typically from a nib.
-        
+
         self.linksTableView.dataSource = self;
         self.linksTableView.delegate = self;
 
+        self.feedbackTableView.dataSource = self;
+
+        
         // [START create_database_reference]
         ref = FIRDatabase.database().reference()
         // [END create_database_reference]
@@ -102,6 +105,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         fetchAndDisplayPersonDescription()
         // fetchAndDisplayPersonArticles()
         // fetchAndDisplayPersonPhotos()
+        fetchAndListenPersonFeedbacks()
     }
 
 
@@ -176,6 +180,21 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // [END read_data_once]
     }
 
+    // MARK: Listen data on Firebase
+    func fetchAndListenPersonFeedbacks() -> Void {
+        self.ref.child("person_feedbacks").child(personUID).observeEventType(FIRDataEventType.Value, withBlock: { (snapshot) in
+
+            if let value = snapshot.value {
+                let dict = value as! NSDictionary
+
+                self.feedbacks = dict.allValues as! [[String: AnyObject]]
+                self.feedbackTableView.reloadData()
+            }
+            else {
+                // TODO: hide table here
+            }
+        })
+    }
 
     // MARK: Write data to Firebase
     func saveFeedback(message: String, uid: String, facebookFriendsCount: Int, completion: () -> Void) -> Void {
@@ -213,37 +232,59 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 
 
-    // MARK: Table View Data Source Methods of Article links
+    // MARK: Table View Data Source Methods
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return articles.count
+        if tableView == self.linksTableView {
+            return articles.count
+        }
+        if tableView == self.feedbackTableView {
+            return feedbacks.count
+        }
+        return 0
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier(articleLinkCellID, forIndexPath: indexPath) as! linksTableViewCell
+        if tableView == self.linksTableView {
+            let cell = tableView.dequeueReusableCellWithIdentifier(articleLinkCellID, forIndexPath: indexPath) as! linksTableViewCell
+            
+            // Fetch Article
+            let article = self.articles[indexPath.row] as [String: String]
+            
+            // Configure Cell
+            cell.linksTitleLabel?.text = article["title"]
+            cell.logoImageView.image = self.logoIconsImageArray[indexPath.row]
+            //  cell.linksBackgroundImageView.image = self.backgroundImagesArray[indexPath.row]
+            return cell
+        }
+        if tableView == self.feedbackTableView {
+            let cell = tableView.dequeueReusableCellWithIdentifier(feedbackCellID, forIndexPath: indexPath) as! FeedbackTableViewCell
 
-        // Fetch Article
-        let article = self.articles[indexPath.row] as [String: String]
+            // Fetch feedback
+            let feedback = self.feedbacks[indexPath.row] as [String: AnyObject]
+            
+            let message = feedback["message"] as! String
+            let followersNumber = feedback["friends_count"] as! Int
 
-        // Configure Cell
-        cell.linksTitleLabel?.text = article["title"]
-        cell.logoImageView.image = self.logoIconsImageArray[indexPath.row]
-      //  cell.linksBackgroundImageView.image = self.backgroundImagesArray[indexPath.row]
-        return cell
+            cell.feedbackTextView.text = message
+            cell.userFollowersNumberLabel.text = String(followersNumber)
+        }
+        return UITableViewCell()
     }
 
 
-    // MARK: Table View Delegate Methods of Article links
+    // MARK: Table View Delegate Methods
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        
-        // Fetch Article
-        let article = self.articles[indexPath.row] as [String: String]
-
-        // LOG
-        print(indexPath.row, article)
+        if tableView == self.linksTableView {
+            // Fetch Article
+            let article = self.articles[indexPath.row] as [String: String]
+            
+            // LOG
+            print(indexPath.row, article)
+        }
     }
 
 
